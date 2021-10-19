1ee9f62b3f2e0264b5d98d8b467729d43bb4983a
nerdeaux <vusi.sizani@gmail.com>
2021-10-19 23:59:53 +0200

    create channel

---
+++ client/src/components/TeamMessage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MessageTeam, useMessageContext } from 'stream-chat-react';
+
+const TeamMessage = () => {
+    const { handleOpenThread, message } = useMessageContext();
+
+    return (
+        <MessageTeam
+            message={{ ...message, user: {}}}
+        />
+    )
+}
+
+export default TeamMessage
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -38,7 +38,9 @@ const login = async (req, res) => {
 
         const { users } = await client.queryUsers({ name: username });
 
-        if(!users.length) return res.status(400).json({ message: 'User not found' });
+        if(!users.length) {
+            return res.status(400).json({ message: 'Username not found' });
+        }
 
         const success = await bcrypt.compare(password, users[0].hashedPassword);
 
